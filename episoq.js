de824d4fe4df99f85e2383b0a7ddb1db90fa267f
Toshiyuki Masui <masui@pitecan.com>
2018-11-08 23:06:47 +0900

    JSON利用

--- episoq.js
+++ episoq.js
@@ -3,24 +3,29 @@ javascript: (() => {
   if(a[2] != 'scrapbox.io') return;
   const project = a[3];
   const page = a[4];
-  const url = `https://scrapbox.io/api/pages/${project}/${page}/text`;
+  const url = `https://scrapbox.io/api/code/${project}/${page}/episopass.json`;
   $.ajax({
     url: url,
     async: false,
     success: (data) => {
-      lines = data.split(/\n/);
-      lines = lines.filter(line => line.match(/^ /));
-      lines = lines.map(line => line.replace(/^ /,''));
-      n = lines.length;
+      questions = data.questions;
+      n = questions.length;
       while (n) {
         const i = Math.floor(Math.random() * n--);
-        [lines[i], lines[n]] = [lines[n], lines[i]]
+        [questions[i], questions[n]] = [questions[n], questions[i]]
       }
       w = window.open();
-      w.document.write("<pre>"+lines.slice(0,10).join("\n")+"</pre>");
+      w.location.href = `http://EpisoPass.com/easy.html?questions=${data.questions.slice(0,10).join(';')}&answers=${data.answers.join(';')}`
     }
   });
 })();
+
+
+      
+      w = window.open();
+      w.document.write("<pre>"+data.questions.slice(0,10).join("\n")+"</pre>");
+      w.document.write("<pre>"+data.answers.join("\n")+"</pre>");
+
 
 
 
