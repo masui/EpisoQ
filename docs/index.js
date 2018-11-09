d7bee352931d2513c21c99d945827f077ac1e900
Toshiyuki Masui <masui@pitecan.com>
2018-11-09 15:55:09 +0900

    ...

--- docs/index.js
+++ docs/index.js
@@ -1,33 +1,36 @@
-function loadScript (url) {
+function loadScript (qa) {
   return new Promise((resolve) => {
-    const script = document.createElement('script')
-    script.src = url
-    script.onload = resolve
-    document.body.appendChild(script)
-  })
+      const script = document.createElement('script');
+      script.src = qa;
+      script.onload = resolve;
+      document.body.appendChild(script);
+  });
 }
 
-async function loadAllScripts (urls) {
-  for (let url of urls) {
-    await loadScript(url)
-  }
+async function aaa(){
+    questions = qa.questions;
+    n = questions.length;
+    while (n) {
+        const i = Math.floor(Math.random() * n--);
+        [questions[i], questions[n]] = [questions[n], questions[i]];
+    }
+    w = window.open();
+    w.location.href = `http://EpisoPass.com/easy.html?questions=${qa.questions.slice(0,10).join(';')}&answers=${qa.answers.join(';')}`;
+}
+
+async function load (url) {
+    await loadScript(url);
+    await aaa();
 }
 
 $(function(){
-  const args = {}
-  document.location.search.substring(1).split('&').forEach((s) => {
-    let [name, value] = s.split('=')
-    args[name] = decodeURIComponent(value)
-  })
-  const codelist = args['qa']
-
-  console.log('codelist', codelist)
-
-  if (codelist) {
-      let urls = codelist.split(/,/)
-      alert(urls)
-    loadAllScripts(urls)
-  }
-
-    alert(qa)
-})
+    const args = {};
+    document.location.search.substring(1).split('&').forEach((s) => {
+	let [name, value] = s.split('=');
+	args[name] = decodeURIComponent(value);
+    });
+    const qa = args['qa'];
+    if (qa) {
+	load(qa);
+    }
+});
